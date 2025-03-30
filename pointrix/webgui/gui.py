383f56f9b407b340454e699246d7215de5589a86
Qingwen <kinzhangglimmer@gmail.com>
2025-03-30 22:30:03 +0100

    hotfix: gaussian_flow gui.

--- pointrix/webgui/gui.py
+++ pointrix/webgui/gui.py
@@ -18,9 +18,11 @@ from ..model.base_model import BaseModel
 
 import viser
 import viser.transforms as tf
+from viser.theme import TitlebarButton, TitlebarConfig, TitlebarImage
 
 from ..utils.registry import Registry
 from ..utils.base import BaseModule, BaseObject
+from ..utils.visualize import visualize_depth
 
 GUI_REGISTRY = Registry("GUI", modules=["pointrix.webgui"])
 GUI_REGISTRY.__doc__ = ""
@@ -56,71 +58,66 @@ class BaseGUI(BaseObject):
         self.rotation = model.point_cloud.get_rotation
         self.shs = model.point_cloud.get_shs
         
+        
         self.render_times = deque(maxlen=3)
         self.server = viser.ViserServer(port=self.cfg.viewer_port)
         self.reset_view_button = self.server.add_gui_button("Reset View")
         
+        self.rgb_button = self.server.add_gui_button("rgb")
+        self.depth_button = self.server.add_gui_button("depth")
+        
         self.server.on_client_disconnect(self._handle_client_disconnect)
+        
+        self.display_mode = "rgb"
         
         self.need_update = False
         self.pause_training = False
         
+        fx = model.training_camera_model.intrs[0].cpu().numpy()[0]
+        fy = model.training_camera_model.intrs[1].cpu().numpy()[0]
+
         self.train_viewer_update_period_slider = self.server.add_gui_slider(
-            "Train Viewer Update Period",
+            "Gaussian Point Cloud Update Period",
             min=1,
             max=100,
             step=1,
             initial_value=10,
             disabled=self.pause_training,
         )
+        @self.rgb_button.on_click
+        def _(event: viser.GuiEvent):
+            if event.client is None:
+                return
+            self.display_mode = "rgb"
         
-        self.pause_training_button = self.server.add_gui_button("Pause Training")
-        self.sh_order = self.server.add_gui_slider(
-            "SH Order", min=1, max=4, step=1, initial_value=1
-        )
+        @self.depth_button.on_click
+        def _(event: viser.GuiEvent):
+            if event.client is None:
+                return
+            if not model.cfg.renderer.render_depth:
+                print("Depth rendering is disabled in the renderer, please enable it")
+            self.display_mode = "depth"
+        
         self.resolution_slider = self.server.add_gui_slider(
-            "Resolution", min=384, max=4096, step=2, initial_value=1024
+            "Resolution", min=512, max=4096, step=2, initial_value=1024
         )
-        self.time_slider = self.server.add_gui_slider(
-            "Time", min=0, max=1, step=0.01, initial_value=0
-        )
-        self.near_plane_slider = self.server.add_gui_slider(
-            "Near", min=0.1, max=30, step=0.5, initial_value=0.1
-        )
-        self.far_plane_slider = self.server.add_gui_slider(
-            "Far", min=30.0, max=1000.0, step=10.0, initial_value=1000.0
-        )
-
-        self.show_train_camera = self.server.add_gui_checkbox(
-            "Show Train Camera", initial_value=False
-        )
-
-        self.fps = self.server.add_gui_text("FPS", initial_value="-1", disabled=True)
         
-        @self.show_train_camera.on_update
-        def _(_):
-            self.need_update = True
+        self.fx_slider = self.server.add_gui_slider(
+            "fx", min=0, max=2 * fx, step=0.1, initial_value=fx
+        )
+        
+        self.fy_slider = self.server.add_gui_slider(
+            "fy", min=0, max=2 * fy, step=0.1, initial_value=fy
+        )
+        self.fps = self.server.add_gui_text("FPS", initial_value="-1", disabled=True)
+        self.num_points = self.server.add_gui_text("Num Points", initial_value=str(len(model.point_cloud.position)), disabled=True)
 
-        @self.resolution_slider.on_update
-        def _(_):
-            self.need_update = True
-
-        @self.near_plane_slider.on_update
-        def _(_):
-            self.need_update = True
-
-        @self.far_plane_slider.on_update
-        def _(_):
-            self.need_update = True
-
-        @self.pause_training_button.on_click
-        def _(_):
-            self.pause_training = not self.pause_training
-            self.train_viewer_update_period_slider.disabled = not self.pause_training
-            self.pause_training_button.name = (
-                "Resume Training" if self.pause_training else "Pause Training"
-            )
-
+        @self.server.on_client_connect
+        def _(client: viser.ClientHandle):
+            @client.camera.on_update
+            def _(_):
+                self.need_update = True
+        
         @self.reset_view_button.on_click
         def _(_):
             self.need_update = True
@@ -128,21 +125,28 @@ class BaseGUI(BaseObject):
                 client.camera.up_direction = tf.SO3(client.camera.wxyz) @ np.array(
                     [0.0, -1.0, 0.0]
                 )
-        
-        self.c2ws = []
-        self.camera_infos = []
-        
-        @self.resolution_slider.on_update
-        def _(_):
-            self.need_update = True
-
-        @self.server.on_client_connect
-        def _(client: viser.ClientHandle):
-            @client.camera.on_update
-            def _(_):
-                self.need_update = True
 
         self.debug_idx = 0
+        
+        buttons = (
+            TitlebarButton(
+                text="Pointrix",
+                icon="GitHub",
+                href="https://github.com/pointrix-project/pointrix",
+            ),
+        )
+        image = TitlebarImage(
+            image_url_light="https://viser.studio/latest/_static/logo.svg",
+            image_alt="Logo",
+            href="https://github.com/nerfstudio-project/viser"
+        )
+        titlebar_theme = TitlebarConfig(buttons=buttons, image=image)
+        brand_color = self.server.add_gui_rgb("Brand color", (10, 10, 10), visible=False)
+        self.server.configure_theme(
+            titlebar_content=titlebar_theme,
+            show_logo=True,
+            brand_color=brand_color.value,
+        )
         
     def _handle_client_disconnect(self, client):
         self.need_update = False
@@ -165,8 +169,8 @@ class BaseGUI(BaseObject):
 
                 W = self.resolution_slider.value
                 H = int(self.resolution_slider.value/camera.aspect)
-                focal_x = W/2/np.tan(camera.fov/2)
-                focal_y = H/2/np.tan(camera.fov/2)
+                focal_x = self.fx_slider.value
+                focal_y = self.fy_slider.value
 
                 start_cuda = torch.cuda.Event(enable_timing=True)
                 end_cuda = torch.cuda.Event(enable_timing=True)
@@ -189,17 +193,20 @@ class BaseGUI(BaseObject):
                     }
                 
                 render_results = self.model.renderer.render_batch(render_dict)
-                
                 end_cuda.record()
                 torch.cuda.synchronize()
                 interval = start_cuda.elapsed_time(end_cuda)/1000.
 
-                out = render_results["rgb"].squeeze().permute(1, 2, 0).cpu().detach().numpy().astype(np.float32)
+                if self.display_mode == 'rgb':
+                    out = render_results["rgb"].squeeze().permute(1, 2, 0).cpu().detach().numpy().astype(np.float32)
+                elif self.display_mode == 'depth':
+                    out = render_results["depth"].squeeze()
+                    out = visualize_depth(out, tensorboard=False)
                 client.set_background_image(out, format="jpeg")
             
             if interval is not None:
                 self.render_times.append(interval)
                 self.fps.value = f"{1.0 / np.mean(self.render_times):.3g}"
-                # print(f"Update time: {end - start:.3g}")
+                self.num_points.value = str(len(self.position))
         
         
